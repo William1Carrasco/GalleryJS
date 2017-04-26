f14442d27a44a70c93ec26b24c097561e8595810
Unknown <wcarrasco@mail.csuchico.edu>
2017-04-25 22:42:43 -0700

    commit

--- gallery.js
+++ gallery.js
@@ -1,6 +1,14 @@
-
+var chico = ["The Bear", "The Flumes", "Upper Bidwell Park", "The Senator", "Rileys"];
+var current = 0,
 
 function shuffleGallery()
 {
-
+  do {
+    var index = Math.floor(Math.random() * (chico.length));
+    current = chico[index];
+    if (current != index) {
+      console.log(current);
+    }
+  }
+  while(current != chico[index]);
 }
